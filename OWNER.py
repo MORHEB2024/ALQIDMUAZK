e692eb99cd22c468c6896e890ac62d8ecb66c7a6
MORHEB2024 <163046774+MORHEB2024@users.noreply.github.com>
2024-04-08 04:36:38 +0200

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["Q_A_ID", "Q_A_ID"]
+OWNER_NAME = "𝐌𝐀𝐇𝐌𝐎𝐔𝐃𝐀𝐋𝐐𝐀𝐈𝐃"
+BOT_TOKEN = "6831377702:AAEOdH1EsQAaOt8B1cwQ18ipNVAFrTMZg4E"
+DATABASE = "mongodb+srv://ahmedfraon:fraon@programmerfraon.n69lkx4.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/SO_Qaid"
+GROUP = "https://t.me/GRQaid"
+VIDEO = "https://t.me/WeAaid/10"
+LOGS = "ALH_KAR"
+
